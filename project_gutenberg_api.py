d7e33320a3021dea209e26473e6aff6dba910110
= <matthew.mattei3@gmail.com>
2022-07-08 15:27:25 -0500

    Updated accessBook() to retrieve all standard book files available.

--- project_gutenberg_api.py
+++ project_gutenberg_api.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 class pgAPI:
     def __init__(self):
         self.html_file = None
+        self.openURL = "gutenberg.org"
 
     def _pageLoader(self, url):
         try:
@@ -27,10 +28,10 @@ class pgAPI:
         #Note, the list is truncated, starting from index 2, to leave out unnecessary link information
         #Saves information in dictionary in the following manner: (book link, image link, title, author, downloads)
         searchList = soup.find_all(class_="booklink")
-        openURL = "gutenberg.org"
+
         for bookInfo in searchList:
-            listOfBooks.append({"book_link" : openURL + bookInfo.find("a", href=re.compile("ebook")).get("href"),
-                                "image_link" : openURL + bookInfo.find("img").get("src"),
+            listOfBooks.append({"book_link" : self.openURL + bookInfo.find("a", href=re.compile("ebook")).get("href"),
+                                "image_link" : self.openURL + bookInfo.find("img").get("src"),
                                 #some titles on project gutenberg include \r, should it be removed?
                                 "title" : bookInfo.find(class_="title").text,
                                 "author" : bookInfo.find(class_="subtitle").text,
@@ -52,36 +53,38 @@ class pgAPI:
         soup = self._pageLoader(url)
         if "Error, web request" in soup:
             return soup
+        bookDetails = {}
         try:
-            coverPicture = soup.find("img", class_="cover-art").get("src")
+            bookDetails["coverPicture"] = soup.find("img", class_="cover-art").get("src")
         except:
-            coverPicture = "No cover available."
+            bookDetails["coverPicture"] = "No cover available."
+        files = []
         try:
-            html = soup.find("a", class_="link", attrs={'type': re.compile("html")}).get("href")
+            for file in soup.find_all(class_="unpadded icon_save"):
+                files.append({file.find("a", title=re.compile("Download")).text : self.openURL+file.find("a", title=re.compile("Download")).get("href")})
         except:
-            html = "HTML book file cannot be found."
-        try:
-            epubImages = soup.find("a", class_="link", attrs={'href': re.compile("epub.images")})
-        except:
-            epubImages = "EPUB book file with images cannot be found."
-        try:
-            epubPlain = soup.find("a", class_="link", attrs={'href': re.compile("epub.noimages")})
-        except:
-            epubPlain = "EPUB book file without images cannot be found"
-        try:
-            alsoDownloaded = (soup.find("div", id="more_stuff").find("a", rel="nofollow").get("href"))
-        except:
-            alsoDownloaded = "Also downloaded link cannot be found"
-        bookAttrs = [coverPicture, html, epubImages, epubPlain, alsoDownloaded]
-        try:
-            author = soup.find("a", itemprop="creator").text
-        except:
-            author = "Author cannot be found"
-        try:
-            translator = soup.find("th", text=re.compile("Translator")).find_next_sibling().text
-        except:
-            translator = "No translator or no translator found."
+            files = "No book files found."
+        bookDetails["book_files"] = files
+        # try:
+        #     epubImages = soup.find("a", class_="link", attrs={'href': re.compile("epub.images")})
+        # except:
+        #     epubImages = "EPUB book file with images cannot be found."
+        # try:
+        #     epubPlain = soup.find("a", class_="link", attrs={'href': re.compile("epub.noimages")})
+        # except:
+        #     epubPlain = "EPUB book file without images cannot be found"
+        # try:
+        #     alsoDownloaded = (soup.find("div", id="more_stuff").find("a", rel="nofollow").get("href"))
+        # except:
+        #     alsoDownloaded = "Also downloaded link cannot be found"
+        # bookAttrs = [coverPicture, html, epubImages, epubPlain, alsoDownloaded]
+        # try:
+        #     author = soup.find("a", itemprop="creator").text
+        # except:
+        #     author = "Author cannot be found"
+        # try:
+        #     translator = soup.find("th", text=re.compile("Translator")).find_next_sibling().text
+        # except:
+        #     translator = "No translator or no translator found."
 
-
-abc = pgAPI()
-
+pgAPI().accessBook("https://gutenberg.org/ebooks/68462")
