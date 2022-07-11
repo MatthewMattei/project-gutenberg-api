5203f52d6202c7898ed98683091f0b6e91d787ef
= <matthew.mattei3@gmail.com>
2022-07-11 13:14:00 -0500

    Work begun on developing flask app for API.

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from project_gutenberg_api import pgAPI
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
+def test():
     return 'Server Works!'
 
 
-@app.route('/greet')
-def say_hello():
-    return 'Hello from Server'
+@app.route('/api', methods = ['POST', 'GET'])
+def api():
+    if request.method == "GET":
+        return "Error, API accessed directly."
+    if request.method == "POST":
+        api = pgAPI()
+        form_data = request.form
