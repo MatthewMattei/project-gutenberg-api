d4a425d3f5e460fb88b874a8a39d2f478127d4e4
= <matthew.mattei3@gmail.com>
2022-09-06 17:22:30 -0500

    New assets and style added to the API HTML page. #TODO: Add form functionality for API testing and add error landing page.

---
+++ app.py
@@ -0,0 +1,17 @@
+from project_gutenberg_api import pgAPI
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template("index.html")
+
+
+@app.route('/api', methods = ['POST', 'GET'])
+def api():
+    if request.method == "GET":
+        return "Error, API accessed directly."
+    if request.method == "POST":
+        api = pgAPI()
+        form_data = request.form
